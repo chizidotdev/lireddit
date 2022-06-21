687715c808fc00b70f9b02d44789cf22b4405c09
chiziivictor <chizivwokoma@gmail.com>
2022-06-22 00:42:22 +0100

    complete graphql crud operations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   // Connect to Database
@@ -18,9 +19,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
 
   await apolloServer.start();
@@ -30,10 +32,7 @@ const main = async () => {
     console.log("Server running on port 4000");
   });
 
-  // // Run SQL
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
-
+  // // Fetch posts
   // const posts = await orm.em.find(Post, {});
   // console.log(posts);
 };
